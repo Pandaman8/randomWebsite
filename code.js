e701b9c06ef8c9fdd733a1c6269a4dcf56d59f23
Tony Tam <steelix.tam@gmail.com>
2021-12-06 00:27:25 -0500

    EVEN MORE CONTENT

--- code.js
+++ code.js
@@ -1,5 +1,7 @@
 function moveButton(){
     var x = document.getElementById("#click1");
+    var tauntTop;
+    var tauntLeft;
     var counterElement = document.getElementById("#counter");
     var counterStr = counterElement.style.right;
     var counter = parseInt(counterStr);
@@ -13,15 +15,27 @@ function moveButton(){
     var leftStr = x.style.left;
     var topInt;
     var leftInt;
+    var tauntNum = 0;
 
     height = Math.min(Cheight, Wheight);
     width = Math.min(Cwidth, Wwidth);
 
     topInt = 20 + (Math.random()*(height-200));
     leftInt = 20 + (Math.random() * (width-200));
+    tauntLeft = 20 + (Math.random() * (width-200));
+    tauntTop = 20 + (Math.random()*(height-200));
+    tauntNum = Math.floor(1 + (Math.random()*5));
+
+    for(let i = 1; i < 6; i++){
+        document.getElementById("#taunt" + i.toString()).style.visibility = "hidden";
+    }
+   
     if(counter < 100){
         x.style.top = topInt.toString() + "px";
         x.style.left = leftInt.toString() + "px";
+        document.getElementById("#taunt" + tauntNum.toString()).style.visibility = "visible";
+        document.getElementById("#taunt" + tauntNum.toString()).style.top = tauntTop.toString() + "px";
+        document.getElementById("#taunt" + tauntNum.toString()).style.left = tauntLeft.toString() + "px";
     }
     counter = counter + 1;
     counterElement.style.right = counter.toString() + "px";
